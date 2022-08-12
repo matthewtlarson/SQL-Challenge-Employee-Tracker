06af36a1ca0884a4ee188c9d223cc3176385a4eb
Matthew Larson <matthewtlarson@email.arizona.edu>
2022-08-11 17:53:18 -0700

    seeds debugging

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,20 +1,20 @@
-INSERT INTO department (id, name)
-VALUES (001, "Employee"),
-       (002, "Manager"),
-       (003, "Intern"),
-       (004, "Executive"),
-       (005, "Boss");
+INSERT INTO department (name)
+VALUES ("Employee"),
+       ("Manager"),
+       ("Intern"),
+       ("Executive"),
+       ("Boss");
        
-INSERT INTO role (id, name, salary, department_id)
-VALUES (001, "Employee1", 25.6, 001),
-       (002, 002, "Manager1" 35.6, 001),
-       (003, 003, "Intern1" 15.6, 001),
-       (004, 004, "Executive1" 45.6, 001),
-       (005, 005, "Boss1" 55.6, 001);
+INSERT INTO role (name, salary, department_id)
+VALUES ("Employee1", 25000, 001),
+       ("Manager1", 35000, 002),
+       ("Intern1", 15000, 003),
+       ("Executive1", 45000, 004),
+       ("Boss1", 55000, 005);
 
-INSERT INTO employee (id, first_name, last_name, role_id)
-VALUES (001, "Matthew", "Larson", 001),
-       (002, "Joe", "Shmoe", 002),
-       (003, "Elon", "Musk", 003),
-       (004, "Bill", "Gates", 004),
-       (005, "Penny", "Hardaway", 005);
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
+VALUES ("Matthew", "Larson", 001, 001),
+       ("Joe", "Shmoe", 002, 002),
+       ("Elon", "Musk", 003, 003),
+       ("Bill", "Gates", 004, 004),
+       ("Penny", "Hardaway", 005, 005);
--- index.js
+++ index.js
@@ -2,11 +2,11 @@ const mysql = require("mysql");
 const inquirer = require("inquirer");
 const table = require('console.table');
 
-//const server = mysql.createConnection({ 
-//   host: "localhost",
-//   port: "3001",
-//   user: "root",
-//   password: "Bggdrub913!",
-//   database: "user_db"
-// });
+const server = mysql.createConnection({ 
+  host: "localhost",
+  port: "3001",
+  user: "root",
+  password: "Bggdrub913!",
+  database: "user_db"
+});
 
